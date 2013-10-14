50295789f6bb95e7b1ac68e24234d05fdf167afe
Mason Stewart <masondesu@gmail.com>
2013-10-14 11:40:49 -0400

    GRRAT FEATURE"

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,19 @@
 /*global describe, it */
 'use strict';
 (function () {
+    describe('Tell me more about pizza cats...', function(){
+      it('PIZZA CATS', function () {
+        expect(3).to.equal(3)
+      });
+    })
+
     describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
             it('should run here few assertions', function () {
-
+              expect(true).to.equal(false)
             });
         });
     });
+
+
 })();
